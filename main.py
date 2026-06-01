d6233b87fe42aab91ac5dba5b711d6c053e96f51
Adel <adeljobygit@gmail.com>
2026-06-01 12:55:19 +0530

    add

---
+++ main.py
@@ -0,0 +1,12 @@
+# This is a sample Python script.
+
+# Press Ctrl+F5 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+a = 10
+b = 3
+print(a/b)
+# See PyCharm help at ht
+#
+# tps://www.jetbrains.com/help/pycharm/
